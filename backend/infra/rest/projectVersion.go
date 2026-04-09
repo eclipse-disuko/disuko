74b3b7cb3c8f4c9ab0a5b276dc533fba38a4cdd9
hfaraji <hesam.faraji@mercedes-benz.com>
2026-04-09 14:38:40 +0200

    fix: Remove unused variable in ProjectVersionSPDXMetaByIDExtern function

--- backend/infra/rest/projectVersion.go
+++ backend/infra/rest/projectVersion.go
@@ -1545,7 +1545,7 @@ func ValidateIDOrLatest(escaped string) string {
 //	@security	Bearer
 func (projectHandler *ProjectHandler) ProjectVersionSPDXMetaByIDExtern(w http.ResponseWriter, r *http.Request) {
 	requestSession := logy.GetRequestSession(r)
-	currentProject, version, _ := projectHandler.retrieveProjectAndVersionFromPublicRequest(requestSession, r)
+	_, version, _ := projectHandler.retrieveProjectAndVersionFromPublicRequest(requestSession, r)
 
 	sbomUuidEscaped := chi.URLParam(r, "sbomUuid")
 	sbomUuid := ValidateIDOrLatest(sbomUuidEscaped)
