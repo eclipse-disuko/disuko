dc8063945a145597620bd9adeacb803da41af400
Chi Nguyen <minh_chi.nguyen@mercedes-benz.com>
2026-04-20 14:42:24 +0200

    feat: Display timezone for all timestamps

--- frontend/libs/shared/utils/constant.ts
+++ frontend/libs/shared/utils/constant.ts
@@ -4,6 +4,6 @@
 
 export const TOOLTIP_OPEN_DELAY_IN_MS = 500;
 export const DATE_FORMAT = 'DD.MM.YYYY';
-export const DATE_FORMAT_SHORT = 'DD.MM.YYYY HH:mm';
-export const DATETIME_FORMAT = 'DD.MM.YYYY HH:mm:ss';
+export const DATE_FORMAT_SHORT = 'DD.MM.YYYY HH:mm [(UTC]Z[)]';
+export const DATETIME_FORMAT = 'DD.MM.YYYY HH:mm:ss [(UTC]Z[)]';
 export const DATETIME_FORMAT_FILE = 'YYYY-MM-DD-HH-mm';
