ced57030c00779c146284b434fe5652c13d097f5
Marcel Wagner <marcel.wagner@mercedes-benz.com>
2026-04-28 14:19:51 +0200

    chore: Updating Header

--- frontend/libs/shared/utils/theme.ts
+++ frontend/libs/shared/utils/theme.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Mercedes-Benz Group AG and Mercedes-Benz AG
+//
+// SPDX-License-Identifier: Apache-2.0
+
 export function getDefaultTheme(): string {
   const theme = localStorage.getItem('disco-theme');
   if (theme === 'dark' || theme === 'light') {
