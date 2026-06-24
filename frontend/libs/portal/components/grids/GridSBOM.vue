926c44c82ee4ea1cb2fdc84d864c20f78b5803d6
REBEKOC <rebecca.r.koch@mercedes-benz.com>
2026-06-24 12:36:31 +0200

    add approvable column #3192

--- frontend/libs/portal/components/grids/GridSBOM.vue
+++ frontend/libs/portal/components/grids/GridSBOM.vue
@@ -113,14 +113,14 @@ const headers = computed((): DataTableHeader[] => [
   },
   ...(!props.channelView
     ? [
-      {
-        title: t('COL_SBOM_BRANCH'),
-        align: 'start',
-        value: 'versionName',
-        sortable: true,
-        width: 110,
-      } as DataTableHeader,
-    ]
+        {
+          title: t('COL_SBOM_BRANCH'),
+          align: 'start',
+          value: 'versionName',
+          sortable: true,
+          width: 110,
+        } as DataTableHeader,
+      ]
     : []),
   {
     title: t('COL_SBOM_TAG'),
@@ -528,24 +528,24 @@ onMounted(async () => {
             <span>{{ item._key }}</span>
             <br v-if="item.approvalInfo && item.approvalInfo.status" />
             <span class="font-weight-bold" v-if="item.approvalInfo && item.approvalInfo.status">{{
-                t(`SBOM_STATUS_${item.approvalInfo.status}`)
-              }}</span>
+              t(`SBOM_STATUS_${item.approvalInfo.status}`)
+            }}</span>
             <span v-if="item.approvalInfo && item.approvalInfo.status">
               <v-icon
                 small
                 v-if="item.approvalInfo && item.approvalInfo.comment && item.approvalInfo.comment.length > 0"
-              >chevron_right</v-icon
+                >chevron_right</v-icon
               >
               {{ item.approvalInfo.comment }}</span
             >
             <br v-if="item.isToDelete" />
             <span v-if="item.isToDelete" class="font-weight-bold text-[rgb(var(--v-theme-error))]">{{
-                t('SBOM_ABOUT_DELETION_NOTE')
-              }}</span>
+              t('SBOM_ABOUT_DELETION_NOTE')
+            }}</span>
             <br v-if="item.isToRetain" />
             <span v-if="item.isToRetain" class="font-weight-bold text-[rgb(var(--v-theme-success))]">{{
-                t('SBOM_MARKED_FOR_RETENTION')
-              }}</span>
+              t('SBOM_MARKED_FOR_RETENTION')
+            }}</span>
           </template>
           <template #[`item.overallReview`]="{item}">
             <DOverallStateIcon v-if="item.overallReview" :review="item.overallReview" />
@@ -592,7 +592,7 @@ onMounted(async () => {
           <template #[`item.actions`]="{item}">
             <TableActionButtons
               variant="slider"
-              :buttons="getActionButtons(item).filter(button => button.event !== 'setApprovable')"
+              :buttons="getActionButtons(item).filter((button) => button.event !== 'setApprovable')"
               @toggleLock="isOwnerOrDomainAdmin ? toggleLock(item) : undefined"
               @setApprovable="setApprovable(item)"
               @addRemark="openReviewRemarkDialog(item)"
@@ -602,8 +602,8 @@ onMounted(async () => {
           </template>
           <template #[`item.star`]="{item}">
             <TableActionButtons
-              :buttons="[getActionButtons(item).find(button => button.event === 'setApprovable')].filter(Boolean)"
-              @setApprovable="setApprovable(item)"/>
+              :buttons="[getActionButtons(item).find((button) => button.event === 'setApprovable')].filter(Boolean)"
+              @setApprovable="setApprovable(item)" />
           </template>
         </v-data-table>
       </div>
