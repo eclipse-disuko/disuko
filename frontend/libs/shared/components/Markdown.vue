a58c6e0473c7668b86642682eb02cf37079d2b71
Chi Nguyen <6671583+cnguyen-de@users.noreply.github.com>
2026-06-25 16:55:54 +0200

    feat: use markdown-it (#251)
    * chore: Update vite 8
    
    * chore: Upgrade to vite 8
    
    * feat: use markdown-it directly

--- frontend/libs/shared/components/Markdown.vue
+++ frontend/libs/shared/components/Markdown.vue
@@ -4,8 +4,7 @@
 
 <script setup lang="ts">
 import MarkdownIt from 'markdown-it';
-import {onMounted, ref} from 'vue';
-import Markdown from 'vue3-markdown-it';
+import {onMounted, ref, watch} from 'vue';
 
 const props = defineProps({
   text: {
@@ -17,32 +16,39 @@ const props = defineProps({
 const md = new MarkdownIt({html: true});
 const markdownText = ref('');
 
-onMounted(() => {
-  // add _target="blank" to all links
-  // see https://github.com/markdown-it/markdown-it/blob/master/docs/architecture.md#renderer
-  const defaultRender =
-    md.renderer.rules.link_open ||
-    function (tokens, idx, options, env, self) {
-      return self.renderToken(tokens, idx, options);
-    };
-  md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-    const aIndex = tokens[idx].attrIndex('target');
-    if (aIndex < 0) {
-      tokens[idx].attrPush(['target', '_blank']);
-    } else {
-      // @ts-expect-error
-      tokens[idx].attrs[aIndex][1] = '_blank';
-    }
-    return defaultRender(tokens, idx, options, env, self);
+const defaultRender =
+  md.renderer.rules.link_open ||
+  function (tokens, idx, options, env, self) {
+    return self.renderToken(tokens, idx, options);
   };
 
+md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+  const aIndex = tokens[idx].attrIndex('target');
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    tokens[idx].attrs![aIndex][1] = '_blank';
+  }
+  return defaultRender(tokens, idx, options, env, self);
+};
+
+onMounted(() => {
   markdownText.value = md.render(props.text);
 });
+
+watch(
+  () => props.text,
+  (newValue) => {
+    if (newValue) {
+      markdownText.value = md.render(newValue);
+    }
+  },
+);
 </script>
 
 <template>
   <div>
-    <Markdown class="markdown" html :source="markdownText" />
+    <div class="markdown" v-html="markdownText"></div>
     <slot />
   </div>
 </template>
