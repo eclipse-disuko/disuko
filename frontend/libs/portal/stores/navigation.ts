cb1c968e67b780a7ceae34e3413a36b7af01f260
hes69 <16311518+hes69@users.noreply.github.com>
2026-08-06 13:18:24 +0200

    refactor: simplify navigation item group creation and sorting logic

--- frontend/libs/portal/stores/navigation.ts
+++ frontend/libs/portal/stores/navigation.ts
@@ -6,9 +6,8 @@ import INavItem from '@disclosure-portal/model/INavItem';
 import ITile from '@disclosure-portal/model/ITile';
 import {useAppStore} from '@disclosure-portal/stores/app';
 import {useUserStore} from '@shared/user/stores/user.store';
-import {sortByAttribute} from '@shared/utils/sort';
 
-export const createNavItemsGroup = function (t: (key: string) => string) {
+export const createNavItemsGroup = function () {
   const rights = useUserStore().getRights;
   const items = [] as INavItem[];
   const adminItems = [] as INavItem[];
@@ -159,10 +158,8 @@ export const createNavItemsGroup = function (t: (key: string) => string) {
     );
   }
 
-  const sortedAdminItems = sortByAttribute(adminItems, 'title', t);
-
   // TODO: Using a store outside of a component, composable or store
-  useAppStore().setNavItemGroup(items, sortedAdminItems);
+  useAppStore().setNavItemGroup(items, adminItems, true);
 
   const res: ITile[] = [];
   const addTile = (title: string, url: string, icon: string, cnt?: number) => {
