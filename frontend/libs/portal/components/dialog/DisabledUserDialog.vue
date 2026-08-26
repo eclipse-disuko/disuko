d96cf17edbd751a9206cf16a8be424865cc7f0b9
Kostyantyn Zagorodniy <kostyantyn.zagorodniy@mercedes-benz.com>
2026-08-26 12:39:41 +0200

    fix: show DisabledUserDialog again  - enhance handling of `USER_DISABLED` response  - migrate DisabledUserDialog to script setup approach  - extend DialogLayout functionality

--- frontend/libs/portal/components/dialog/DisabledUserDialog.vue
+++ frontend/libs/portal/components/dialog/DisabledUserDialog.vue
@@ -2,68 +2,44 @@
 <!---->
 <!-- SPDX-License-Identifier: Apache-2.0 -->
 
-<template>
-  <v-dialog v-model="show" content-class="small" persistent>
-    <v-card class="pa-8 dDialog">
-      <v-card-title>
-        <v-row class="align-center">
-          <v-col>
-            <span class="text-h5">{{ t('DLG_DISABLED_USER_TITLE') }}</span>
-          </v-col>
-        </v-row>
-      </v-card-title>
-      <v-card-text>
-        <v-row>
-          <v-col>
-            <span>{{ t('DLG_DISABLED_USER_TEXT') }}</span>
-          </v-col>
-        </v-row>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <DCActionButton
-          isDialogButton
-          :text="t('BTN_LOGOUT')"
-          icon="logout"
-          :hint="t('TT_DISABLED_USER_LOGOUT')"
-          @click="logoutUser"></DCActionButton>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
-</template>
-
-<script>
+<script setup lang="ts">
 import {useUserStore} from '@shared/user/stores/user.store';
 import {logout} from '@disclosure-portal/utils/logout';
-import DCActionButton from '@shared/components/disco/DCActionButton.vue';
-import {defineComponent, ref} from 'vue';
+import {computed, ref} from 'vue';
 import {useI18n} from 'vue-i18n';
 
-export default defineComponent({
-  name: 'DisabledUserDialog',
-  components: {
-    DCActionButton,
+const {t} = useI18n();
+const userStore = useUserStore();
+const show = ref(false);
+
+const dialogConfig = computed(() => ({
+  title: t('DLG_DISABLED_USER_TITLE'),
+  primaryButton: {
+    text: t('BTN_LOGOUT'),
+    hint: t('TT_DISABLED_USER_LOGOUT'),
+    icon: 'mdi mdi-logout',
+    iconColor: 'default',
   },
-  setup() {
-    const {t} = useI18n();
-    const userStore = useUserStore();
-    const show = ref(false);
+  showCloseButton: false,
+}));
 
-    const open = () => {
-      show.value = true;
-    };
+const open = () => {
+  show.value = true;
+};
 
-    const logoutUser = () => {
-      userStore.clear();
-      logout();
-    };
+const logoutUser = () => {
+  userStore.clear();
+  logout();
+};
 
-    return {
-      open,
-      show,
-      t,
-      logoutUser,
-    };
-  },
-});
+defineExpose({open});
 </script>
+<template>
+  <v-dialog v-model="show" width="600" persistent>
+    <DialogLayout :config="dialogConfig" @primary-action="logoutUser">
+      <Stack>
+        <span>{{ t('DLG_DISABLED_USER_TEXT') }}</span>
+      </Stack>
+    </DialogLayout>
+  </v-dialog>
+</template>
